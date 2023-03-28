64cd579a01f5db2c6b06bcdb5c51ddfa50a62aaf
Nifacia Bell <nifaciabell.tech@gmail.com>
2023-03-28 16:12:14 -0400

    commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+{/* <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav>
+            
+          </Nav>
+        </Container>
+      </Navbar> */}
 
 
+      
 function NavBar() {
   return (
   
