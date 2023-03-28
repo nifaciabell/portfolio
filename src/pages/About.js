ef28f7a41f01c7ea96ec377152781c17449111e3
Nifacia Bell <nifaciabell.tech@gmail.com>
2023-03-28 12:54:47 -0400

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,12 @@
+// import HeadShot from './HeadShot.png';
+
+
 function About() {
   return (
     <div className="App">
     <h1>About Me</h1>
+    {/* <img src={HeadShot} /> */}
+
     <p>Talented, creative college educated UX Designer and Software Engineer with coding and technical expertise. An avid researcher with a keen eye to detail. Ability to work well with others and independently when performing, designing an end-to-end experience providing a well-developed product. A natural born leader with exceptional communication skills and a can-do attitude. Able to manage multiple priorities while maintaining a calm demeanor when addressing technical solutions.</p>
     <br/>
     <button>Check out my Resume</button>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,29 @@
 function Contact() {
   return (
     <div className="App">
-    <h1>Contact</h1>
+      <h1>Contact</h1>
+ <form>
+   <label htmlFor="name">Name:</label>
+   <input type="text" id="name" name="name" placeholder="Enter your name" />
+   </form>
+
+   <form>
+   <label htmlFor="email">Email:</label>
+   <input type="text" id="email" name="email" placeholder="Enter your email address" />
+   </form>
+
+   <form>
+   <label htmlFor="phone">Phone:</label>
+   <input type="text" id="phone" name="phone" placeholder="Enter your phone number" />
+   </form>
+
+   <form>
+   <label htmlFor="message">Message:</label>
+   <input type="text" id="message" name="message" placeholder="Leave a message" />
+   </form>
+	
+
+
     </div>
   );
 }
